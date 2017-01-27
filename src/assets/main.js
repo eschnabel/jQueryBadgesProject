04c94edb3447c01b71e6b25d9c6b1bcba5897862
Evan Schnabel <eschnabel@mac.com>
2017-01-26 18:36:51 -0800

    5-9

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,20 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/evanschnabel.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      var badgesDiv = $('#badges');
+      for (var i = 0; i < response.courses.completed.length; i++) {
+        var course = response.courses.completed[i];
+          badgesDiv.append('<div class="course">' +
+            '<h3>' + course.title + '</h3>' +
+            '<img src="' + course.badge + '"/>' +
+            '<a href="' + course.url + '" target="_blank" class="btn btn-primary">See Course</a>' +
+            '</div>');
+      }
+    }
+  });
 
 });
